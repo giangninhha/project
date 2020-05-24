ceb10e6e24159e2f2ce7857853ae66c41bd0bc05
giangninhha <63857001+giangninhha@users.noreply.github.com>
2020-05-24 18:32:51 +0700

    Update danhmuc.php

--- admin/danhmuc/danhmuc.php
+++ admin/danhmuc/danhmuc.php
@@ -9,7 +9,7 @@
 	  <h4 class="page-title">
 	    <span class="page-title-icon bg-gradient-primary text-white mr-2">
 	      
-	    </span> ADMIN - AN HẠ SHOP&#160;<i class="fas fa-chevron-right" style="font-size: 18px"></i>&#160; Danh Mục</h4>
+	    </span> ADMIN - mixi SHOP&#160;<i class="fas fa-chevron-right" style="font-size: 18px"></i>&#160; Danh Mục</h4>
 	</div><br>
 
 <?php include_once('danhmuc/main.php');?>
