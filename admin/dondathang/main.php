51693a342e2205b02e7bdeaca8eb998f8f4426d5
giangninhha <63857001+giangninhha@users.noreply.github.com>
2020-05-24 18:36:35 +0700

    Update main.php

--- admin/dondathang/main.php
+++ admin/dondathang/main.php
@@ -9,7 +9,7 @@
 	  <h4 class="page-title">
 	    <span class="page-title-icon bg-gradient-primary text-white mr-2">
 	      
-	    </span> ADMIN - AN HẠ SHOP&#160;<i class="fas fa-chevron-right" style="font-size: 18px"></i>&#160; Đơn Đặt Hàng</h4>
+	    </span> ADMIN - mixi SHOP&#160;<i class="fas fa-chevron-right" style="font-size: 18px"></i>&#160; Đơn Đặt Hàng</h4>
 	</div>
 	<div class="row">
         <form action="?action=xldathang" method="GET" class="col-md-9 grid-margin stretch-card">
